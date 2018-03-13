c9447546ab2e01587113a70b3504b871e60735a1
Jane Jeon <sungil.ahn.19@dartmouth.edu>
2018-03-13 02:56:05 -0700

    git really fixed

--- src/main/java/util/Load.java
+++ src/main/java/util/Load.java
@@ -1,4 +1,10 @@
 package util;
 
+import java.util.Scanner;
+
 public class Load {
+	// reads in entire file and loads it into a String
+	public static String string(String filename) {
+		return new Scanner(Load.class.getClassLoader().getResourceAsStream(filename)).next();
+	}
 }
